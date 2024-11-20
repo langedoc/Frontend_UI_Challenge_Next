746c54953a9db734557a9399f639534ba8627f51
yulia <langedoc@gmail.com>
2024-11-20 13:36:07 +0100

    layout.jsx updated with routing, App.jsx removed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,20 +1,26 @@
+'use client'
+
 import React from 'react'
 import Header from '../components/Header/Header'
 import WishlistOverlay from '../components/WishlistOverlay/WishlistOverlay'
 import { useMovieContext } from '../contexts/MovieContext'
-
-// 'use client' since hooks are used
-'use client'
 
 export default function RootLayout({ children }) {
   const { isWishlistOpen } = useMovieContext()
 
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Mytheresa Frontend Challenge</title>
+      </head>
       <body>
-        <Header />
-        {children}
-        {isWishlistOpen && <WishlistOverlay />}
+        <MovieProvider>
+          <Header />
+          {children}
+          {isWishlistOpen && <WishlistOverlay />}
+        </MovieProvider>
       </body>
     </html>
   )
